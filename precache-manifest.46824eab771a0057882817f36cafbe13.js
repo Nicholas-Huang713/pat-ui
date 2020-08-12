eaf94c45ae34d181e502ac3063559b0e80482d3e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-12 20:21:48 +0000

    Deploy YuhanLin1105/pat-ui to github.com/YuhanLin1105/pat-ui.git:gh-pages

---
+++ precache-manifest.46824eab771a0057882817f36cafbe13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a245db597363ad23aec58eff584684",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a798acf05b81a61068c4.bundle.js"
+  },
+  {
+    "url": "runtime~main.a798acf05b81a61068c4.bundle.js"
+  },
+  {
+    "revision": "9c25b7c88a05c3a2f41f",
+    "url": "static/css/main.922c0226.chunk.css"
+  },
+  {
+    "revision": "fe19491642283c19e9c7",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.a798acf05b81a61068c4.bundle.js"
+  },
+  {
+    "url": "vendors~main.a798acf05b81a61068c4.bundle.js.LICENSE.txt"
+  }
+]);
